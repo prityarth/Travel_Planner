86ee2efe90df97881bc02bbb51ba96d7bb03ea18
Prityarth Pradhan <prityarth.p.0224@inspiria.edu.in>
2026-04-02 16:40:31 +0530

    .env

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCSG82jHn3MnXUnpiUZGFLNld7-5Z0GPfo",
-  authDomain: "travel-planner-1b496.firebaseapp.com",
-  projectId: "travel-planner-1b496",
-  storageBucket: "travel-planner-1b496.firebasestorage.app",
-  messagingSenderId: "984411209523",
-  appId: "1:984411209523:web:1aab48fbe36152a26a8846",
-  measurementId: "G-0Q98H6SGSX"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
